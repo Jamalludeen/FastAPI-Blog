9105ec90bff7303bf5e1c1aeb64f7a05b0415eb5
Jamalludeen <jamalghazniwal@gmail.com>
2026-07-28 22:40:06 +0430

    add test data

--- main.py
+++ main.py
@@ -2,6 +2,23 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+posts = [
+    {
+        "id": 1,
+        "author": "James Bond",
+        "title": "FastAPI is Awesome",
+        "content": "This is a framework for building APIs",
+        "date_posted": "2025 01 17",
+    },
+    {
+        "id": 2,
+        "author": "John Doe",
+        "title": "Python is Great for backend",
+        "content": "Python is a programming language.",
+        "date_posted": "2023 09 22",
+    }
+]
+
 @app.get("/")
 def home():
     return {"message": "Hello World!"}

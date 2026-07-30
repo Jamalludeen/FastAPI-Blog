739ac7e8c34a307ca4696e8d703409b4805389f8
Jamalludeen <jamalghazniwal@gmail.com>
2026-07-30 10:40:16 +0430

    return template as response

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from fastapi.responses import HTMLResponse
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 
@@ -23,10 +22,11 @@ posts: list[dict] = [
     }
 ]
 
-@app.get("/", response_class=HTMLResponse, include_in_schema=False)
-@app.get("/posts", response_class=HTMLResponse, include_in_schema=False)
-def home():
-    return f"<h1>{posts[0]['title']}</h1>"
+@app.get("/", include_in_schema=False)
+@app.get("/posts", include_in_schema=False)
+def home(request: Request):
+    return templates.TemplateResponse(request, "home.html")
+
 
 
 @app.get("/api/posts")

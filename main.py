528c1446a0f51de66ff83497fca8227989bf2c7e
Jamalludeen <jamalghazniwal@gmail.com>
2026-07-31 20:32:35 +0430

    add HTTP POST method for creating new posts

--- main.py
+++ main.py
@@ -52,6 +52,20 @@ def get_posts():
     return posts
 
 
+@app.post("/api/posts", response_model=PostResponse, status_code=status.HTTP_201_CREATED)
+def create_post(post: PostCreate):
+    new_id = max(p["id"] for p in posts) + 1 if posts else 1
+    new_post = {
+        "id": new_id,
+        "title": post.title,
+        "author":post.author,
+        "content": post.content,
+        "date_posted": "April 23, 2026"
+    }
+    posts.append(new_post)
+    return new_post
+
+
 @app.get("/api/posts/{post_id}", response_model=PostResponse)
 def get_post(post_id: int):
     for post in posts:
